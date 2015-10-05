97b87217385ec29d7c63bec904f0e71f2266f223
Prashant Dixit <nandudixit855@gmail.com>
2015-10-05 23:41:23 +0530

    add validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,6 @@ class Micropost < ActiveRecord::Base
 
 	belongs_to :user
 	validates :cntent, length: { maximum: 140 }
+	validates :cntent, :user_id, presence: true
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :microposts
+
+	validates :name, :email, presence: true
+	validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
