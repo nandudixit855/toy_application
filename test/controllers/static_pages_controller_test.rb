74903f708f9c9004444454b69eacc5d1f387cbe0
Prashant Dixit <nandudixit855@gmail.com>
2015-10-06 09:02:30 +0530

    add static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "ToyApp"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "ToyApp"
+  end
+
+  test "should get about page." do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "ToyApp"
   end
 
 end
