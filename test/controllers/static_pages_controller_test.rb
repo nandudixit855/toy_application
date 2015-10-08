2b08ba1fd23c4d23f669cac9068079c050b38253
Prashant Dixit <nandudixit855@gmail.com>
2015-10-08 08:35:02 +0530

    add static page and its title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "ToyApp"
+    assert_select "title", "ToyApp hello."
   end
 
   test "should get help" do
